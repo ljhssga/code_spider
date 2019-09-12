97e51c4856e4a35f222cbffaa61beb83e9d9335a
LJHsb <1933304310@qq.com>
2019-09-12 11:55:45 +0800

    Update DbClient.py

--- db/DbClient.py
+++ db/DbClient.py
@@ -5,7 +5,7 @@ import uuid
 class DBClient():
     def __init__(self):
         # 打开数据库连接
-        self._conn = pymysql.connect("111.231.137.74", "root", "123456", "spider")
+        self._conn = pymysql.connect("127.0.0.1", "root", "123456", "spider")
         self._cursor = self._conn.cursor()
 
     def _exeCute(self, sql):
